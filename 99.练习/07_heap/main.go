93f2de915e88ac0cc1d95d2efb7fbfaa4a85f404
touch <396223353@qq.com>
2021-12-16 10:41:43 +0800

    f

--- 99.练习/07_heap/main.go
+++ 99.练习/07_heap/main.go
@@ -3,7 +3,19 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"sort"
 )
+
+// 另一用简便写法
+type hp struct{ sort.IntSlice }
+
+func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
 
 type IntHeap []int
 
